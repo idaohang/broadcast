2362c557ee5c1b185a55c7e5e8ba4cc6fb641904
Jacob Adams <jacobaa1031@gmail.com>
2014-07-17 17:05:00 -0400

    began socket stuff

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "PracticalSocket.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+#define HOST "192.168.43.255"
+#define HOST_PORT 4551
+#define GPSFILE "/dev/ttyUSB0"
+int main () {
+	
