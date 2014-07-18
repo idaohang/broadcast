8522d25498ca8477d70a37d8669de14bc42cdda9
Jacob Adams <jacobaa1031@gmail.com>
2014-07-18 09:29:21 -0400

    Minor fixes

--- main.cpp
+++ main.cpp
@@ -5,24 +5,16 @@
 #include <sstream>
 #include "PracticalSocket.h"
 using namespace std;
+
 #define HOST "192.168.43.255"
 #define HOST_PORT 4551
 #define GPSFILE "/dev/ttyUSB0"
-// From http://stackoverflow.com/a/1494435
-void Replace(std::string& str, const std::string& oldStr, const std::string& newStr) {
-	size_t pos = 0;
-	while((pos = str.find(oldStr, pos)) != std::string::npos) {
-		str.replace(pos, oldStr.length(), newStr);
-                pos += newStr.length();
-	}
-}
 
 int main () {
 	string line;
 	ifstream gpsdata;
 	gpsdata.open(GPSFILE);
 	while(getline(gpsdata,line)) {
-		Replace(line,","," ");
 		found = line.find("$GPRMC");
 		if (found != string::npos) {
 			cout << "found data: " << line;
@@ -33,7 +25,6 @@ int main () {
 			 catch (SocketException &e) {
     				cerr << e.what() << endl;
     			}
-			}
 		}
 			sleep(0.1);
 	}
