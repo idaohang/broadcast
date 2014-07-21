bedac13088d879875b26c19c8983ded65dbd0bce
Jacob Adams <jacobaa1031@gmail.com>
2014-07-21 08:50:57 -0400

    made some changes

--- main.cpp
+++ main.cpp
@@ -1,34 +1,47 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <unistd.h>
-#include <cstdio>
-#include <sys/socket.h>
+#include "PracticalSocket.h"
 using namespace std;
 #define GPSFILE "/dev/ttyUSB0"
 
-const string host = "192.168.43.255";
+const string host = "192.168.96.181";
 unsigned short host_port = 4551;
-
 bool bcast (string line) {
-
+	try {
+		UDPSocket sock;
+		cout << "trying udp";
+		sock.sendTo(line.c_str(),line.size(), host, host_port);
+	}
+	catch (SocketException &e) {
+	cerr << e.what() << "\n";
+	return false;
+	}
+	return true;
 }
 int main () {
+	 cout << "running\n";
 	string line;
 	ifstream gpsdata;
 	gpsdata.open(GPSFILE);
-	while(getline(gpsdata,line)) {
-		int found = line.find("$GPRMC");
-		if (found != string::npos) {
-			cout << "found data: " << line;
-			bool test = bcast(line);
-			if (!test) {
-				cerr << "FAIL: " << line;
+	cout << "started";
+	if (gpsdata.is_open()) {
+		while(getline(gpsdata,line)) {
+			cout << "finding data";
+			unsigned int found = line.find("$GPRMC");
+			if (found != string::npos) {
+				cout << "found data: " << line << endl;
+				bool test = bcast(line);
+				if (!test) {
+					cerr << "FAIL: " << line << endl;
+				}
 			}
+				sleep(1);
 		}
-			sleep(1);
+	}
+	else  {
+	cerr << "failure to read file";
 	}
 }
